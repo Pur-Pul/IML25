258d7e8b809564fc53eab0e0871320ab84900ecd
Pur-Pul <axbergsamuel@gmail.com>
2025-11-10 12:18:01 +0200

    Progress on E1P2C

--- E1/P2/C.py
+++ E1/P2/C.py
@@ -2,23 +2,40 @@ import numpy as np
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
 import pandas as pd
-from ISLP.models import (ModelSpec as MS, poly)
+from math import sqrt
+from ISLP.models import (ModelSpec as MS, poly, sklearn_sm)
+from sklearn.model_selection import (cross_validate, KFold, ShuffleSplit)
+from sklearn.dummy import DummyRegressor as Dummy
+from sklearn.ensemble import RandomForestRegressor as RF
 
 def MSE(model, x, y):
-    return ((y - model.predict(x))**2).sum() / len(x)
-
-class dummy():
-    def __init__(self, df):
-        self.mean = np.mean(trainDF['Next_Tmax'])
-
-    def predict(self, x):
-        return self.mean
+    model.fit(x, y)
+    return np.mean((y - model.predict(x))**2)
 
 trainDF = pd.read_csv("data/train_real.csv")
+testDF = pd.read_csv("data/test_real.csv")
+trainX = trainDF.drop(columns=['Next_Tmax'])
+trainY = trainDF['Next_Tmax']
+testX = testDF.drop(columns=['Next_Tmax'])
+testY = testDF['Next_Tmax']
 
-X = trainDF.drop(columns=['Next_Tmax'])
-y = trainDF['Next_Tmax']
+dummyModel = Dummy(strategy='mean')
+RFModel = RF()
+OLSmodel = sklearn_sm(sm.OLS)
+cv = KFold(n_splits=10, shuffle=True, random_state=0)
 
-model = sm.OLS(y, X).fit()
-print(MSE(dummy(trainDF), X, y))
-print(MSE(model, X, y))
+print(
+    MSE(dummyModel, trainX, trainY),
+    MSE(dummyModel, testX, testY),
+    np.mean(cross_validate(dummyModel, trainX, trainY, cv=cv)['test_score'])
+)
+print(
+    MSE(OLSmodel, trainX, trainY),
+    MSE(OLSmodel, testX, testY),
+    np.mean(cross_validate(OLSmodel, trainX, trainY, cv=cv)['test_score'])
+)
+print(
+    MSE(RFModel, trainX, trainY),
+    MSE(RFModel, testX, testY),
+    np.mean(cross_validate(RFModel, trainX, trainY, cv=cv)['test_score'])
+)
